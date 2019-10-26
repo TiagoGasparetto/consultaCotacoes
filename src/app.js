3ba40172b590a67af21f5285ebf8d7961040ef31
Tiago Gasparetto <tiagolgasparetto@gmail.com>
2019-10-25 23:33:48 -0300

    teste commit

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
