20ae1f826e9e1bcaa857e0879107bc5966b5f479
Tiago Gasparetto <tiagolgasparetto@gmail.com>
2019-10-26 00:26:40 -0300

    alteraçao do cliente

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index',{
-        title: 'Cotações da Manu',
+        title: 'Cotações da Manu!',
         author: 'Biharck'
     })
 })
